5a53c798c9629c72557bdaac6c5ffe9549b3f5bd
Margarita-Anisimova <margarita311@autorambler.ru>
2022-03-07 15:56:32 +0500

    change names in resume1

--- job-search/Models/Resumes.cs
+++ job-search/Models/Resumes.cs
@@ -35,12 +35,14 @@ namespace job_search.Models
         [ForeignKey("work_type_id")]
         public Work_type_ref Work_type_ref { get; set; }
 
-        public int ready_move { get; set; }
+        public string ready_move { get; set; }
         public string skills { get; set; }
         [Required]
         public int profession_id { get; set; }
         [ForeignKey("profession_id")]
         public Profession_ref Profession_ref { get; set; }
+
+        public string education_level { get; set; }
 
 
     }
